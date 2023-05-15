7ca607c392c3be3d99dfe35c7bddd359417fa8c5
David R Connell <davidconnell12@gmail.com>
2023-05-15 11:47:41 -0500

    Setup bootstrapping
    - Add seeding
    - Add random functions
    - Change to a static library so we don't have to deal with run time dependencies

--- src/se2_core.c
+++ src/se2_core.c
@@ -1,13 +1,63 @@
 #include <igraph_error.h>
-#include <igraph_random.h>
-#include <igraph_interface.h>
 #include <igraph_structural.h>
 #include <omp.h>
 
 #include "speak_easy_2.h"
+#include "se2_seeding.h"
+#include "se2_random.h"
+
 
 #define SE2_SET_OPTION(opts, field, default) \
     (opts->field) = (opts->field) ? (opts->field) : (default)
+
+static void se2_core(igraph_t *graph, igraph_vector_t const ic_store,
+                     size_t const subcluster_iter, options const *opts, outputs *res);
+
+static void se2_bootstrap(igraph_t *graph, igraph_vector_t const *weights,
+                          igraph_bool_t const directed, size_t const subcluster_iter,
+                          options const *opts, outputs *res)
+{
+  igraph_integer_t n_nodes = igraph_vcount(graph);
+  igraph_vector_t kin;
+
+  igraph_vector_init(&kin, n_nodes);
+  igraph_strength(graph, &kin, igraph_vss_all(), IGRAPH_IN, IGRAPH_NO_LOOPS,
+                  weights);
+
+  if ((!subcluster_iter) && (opts->max_threads > 1)) {
+    puts("starting level 1 clustering; independent runs might not be displayed in order - that is okay");
+  }
+
+  if ((!subcluster_iter) && (opts->multicommunity > 1)) {
+    puts("attempting overlapping clustering");
+  }
+
+  #pragma omp parallel for
+  for (igraph_integer_t run_i = 0; run_i < opts->independent_runs; run_i++) {
+    igraph_vector_int_t ic_store;
+    igraph_vector_int_init(&ic_store, n_nodes);
+
+    se2_rng_init(run_i + opts->random_seed);
+    size_t n_unique = se2_seeding(graph, weights, directed, &kin, opts,
+                                  &ic_store);
+
+    if ((!subcluster_iter) && (run_i == 0)) {
+      printf("produced about %zu seed labels, while goal was %zu\n", n_unique,
+             opts->target_clusters);
+      puts("completed generating initial labels");
+    }
+
+    if (!subcluster_iter) {
+      printf("\nstarting independent run # %zu of %zu\n", run_i + 1,
+             opts->independent_runs);
+    }
+
+    /* se2_core(graph, ic_store, subcluster_iter, kin, opts, res); */
+    igraph_vector_int_destroy(&ic_store);
+  }
+
+  igraph_vector_destroy(&kin);
+}
 
 static size_t default_target_clusters()
 {
@@ -36,7 +86,7 @@ static void se2_set_defaults(options *opts)
   SE2_SET_OPTION(opts, target_clusters, default_target_clusters());
   SE2_SET_OPTION(opts, minclust, 5);
   SE2_SET_OPTION(opts, discard_transient, 3);
-  SE2_SET_OPTION(opts, random_seed, RNG_INTEGER(1, 9999));
+  SE2_SET_OPTION(opts, random_seed, (size_t)RNG_INTEGER(1, 9999));
   SE2_SET_OPTION(opts, max_threads, default_max_threads());
   SE2_SET_OPTION(opts, node_confidence, false);
 }
@@ -47,7 +97,7 @@ int speak_easy_2(igraph_t *graph, igraph_vector_t const *weights,
   printf("\ncalling main routine at level 1\n");
   se2_set_defaults(opts);
 
-  /* se2_bootstrap(graph, weights, directed, 0, opts, res); */
+  se2_bootstrap(graph, weights, directed, 0, opts, res);
 
   if (opts->node_confidence) {
     // pass;
---
+++ src/se2_seeding.h
@@ -0,0 +1,12 @@
+#ifndef SE2_SEEDING_H
+#define SE2_SEEDING_H
+
+#include <igraph_interface.h>
+
+#include "speak_easy_2.h"
+
+size_t se2_seeding(igraph_t const *graph, igraph_vector_t const *weights,
+                   igraph_bool_t const directed, igraph_vector_t const *kin, options const *opts,
+                   igraph_vector_int_t *ic_store);
+
+#endif
